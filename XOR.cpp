424e38b5744ecc366b7045855be4cebcbd932df2
Andras Borbely <kisbogyi2003@gmail.com>
2022-04-20 18:21:09 +0200

    infinite xor with char key

--- XOR.cpp
+++ XOR.cpp
@@ -1,30 +1,19 @@
 #include <iostream>
-int stringtoint(const char* str){
-    int tmp = str[0];
-    for(int i = 1; i < strlen(str); i++){
-        tmp*100;
-        tmp += str[i];
-    }
-    return tmp;
+#include <cstring>
 
-}
-char * inttostring(int number){
-    char *tmp = new char[0]
-    while(number != 0){
-        char *temp;
-        strcat(temp, tmp);
-        delete[] tmp;
-        char *tmp = new char[strlen(temp)+2]
-        strcat(tmp, number % 100);
-        number = number/100;
-    }
-    char *ret;
-    strcat(ret, tmp);
-    delete[] tmp;
-    return ret;
+using namespace std;
+
+const char* XOR(const char *text,char key){     //TODO char is temporyry i will write it over with const char * in the future
+    char *ct = new char[strlen(text)+1];    
+    for(int i = 0; i < strlen(text); i++){
+            char a = text[i] ^ key;
+            ct[i] = a;
+        }
+        return ct;
 }
 
+int main(void){
+    cout << XOR("Alma", 'T') <<  endl;
+    cout << XOR(XOR("Alma", 'T'), 'T') << endl;
 
-char * XOR(const char *key, const char* text){
-    return stringtoint(key) ^ stringtoint(text);
 }
