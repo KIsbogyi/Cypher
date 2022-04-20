9f3afc43da9a6bfb8bb4c621d4066105fa0c2b50
Kisbogyi <55801726+KIsbogyi@users.noreply.github.com>
2022-04-20 13:03:17 +0200

    started stoi

--- XOR.cpp
+++ XOR.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
 
+/*
+class XOR{
+    char *key;
+    char *cyphertext
+
+    public:
+        XOR(){}
+};
+*/
+
+
+int stringtoint(const char* str){
+    int tmp = str[0];
+    for(int i = 1; i < strlen(str); i++){
+        tmp*100;
+        tmp += str[i];
+    }
+    return tmp;
+
+}
+
+char * XOR(const char *key, const char* text){
+    return key ^ text;
+
+}
